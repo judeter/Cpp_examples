c330a8d306c76cb2ac838dbf2394ee495e46509a
Justin Deterding <Justin Deterding>
2020-09-04 07:14:21 -0600

    Added ptr to max value in vector

--- StdRandom/StdRandom.cpp
+++ StdRandom/StdRandom.cpp
@@ -5,9 +5,9 @@
 
 int main(void)
 {
-	std::default_random_engine genorator (13689);
+	std::default_random_engine genorator (189);
 	std::uniform_real_distribution<double> distribution(0.0, 10.0);
-
+	double* maxValue = 0;
 	std::vector<double> values;
 	std::vector<int> bins = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
@@ -16,12 +16,12 @@ int main(void)
 	while (values.size() < 200) { 
 		value = distribution(genorator);
 		values.push_back(value);
+		if (values.size() == 1) { maxValue = &values.back(); }
+		if (values.back() > *maxValue) { maxValue = &values.back(); }
 		index = static_cast<int> (value);
 		bins[index] += 1;
 	}
 	for (int count : bins) { std::cout << std::string(count,'X') << std::endl; }
-
-
-
+	std::cout << "Max Value: " << *maxValue << std::endl;
 
 }
